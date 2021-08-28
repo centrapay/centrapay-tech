f9e26eec34ab9e7e63b3f2a33e88428ad61aee8a
Nathan Jones <nathan@ncjones.com>
2021-08-29 00:37:37 +1200

    Add env config logging

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,13 @@ const pluginRss = require('@11ty/eleventy-plugin-rss');
 const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const pluginNavigation = require('@11ty/eleventy-navigation');
 
+const config = require('./src/config');
+/* eslint-disable-next-line no-console */
+console.log(`
+Resolved Config:
+${require('util').inspect(config.getProperties(), null, null)}
+`);
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.setDataDeepMerge(true);
 
