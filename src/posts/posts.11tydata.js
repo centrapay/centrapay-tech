1e3be6cc9201aead96f784335db1a37c7265c8c2
Nathan Jones <nathan@ncjones.com>
2021-10-03 00:43:01 +1300

    Add estimated reading time

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -7,5 +7,11 @@ module.exports = {
   eleventyComputed: {
     /* replace author id with author object */
     author: data => authors.byId[data.author],
+    readingTime: async data => {
+      const content = await require('fs').promises.readFile(data.page.inputPath, 'utf-8');
+      const wordCount = content.match(/[\w']+/g).length;
+      /* typical reading speed is 250 wpm */
+      return Math.ceil(wordCount / 250);
+    }
   }
 };
